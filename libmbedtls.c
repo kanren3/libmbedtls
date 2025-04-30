7f4ccc3bdc31cf71a2ac6b277cbf08278d08d8be
kanren3 <root@ruriwo.com>
2025-04-30 16:18:23 +0800

    fix wrapper

--- libmbedtls.c
+++ libmbedtls.c
@@ -25,13 +25,27 @@ int libmbedtls_snprintf(char *s, size_t n, const char *format, ...)
 void* libmbedtls_calloc(size_t num, size_t size)
 {
     void* Pointer;
-    Pointer = ExAllocatePoolWithTag(NonPagedPool, num * size, 'mbed');
+    size_t NumberOfBytes;
+
+    Pointer = NULL;
+    NumberOfBytes = num * size;
+
+    if (0 != NumberOfBytes) {
+        Pointer = ExAllocatePoolWithTag(NonPagedPool, NumberOfBytes, 'mbed');
+
+        if (NULL != Pointer) {
+            memset(Pointer, 0, NumberOfBytes);
+        }
+    }
+
     return Pointer;
 }
 
 void libmbedtls_free(void *ptr)
 {
-    ExFreePoolWithTag(ptr, 'mbed');
+    if (NULL != ptr) {
+        ExFreePoolWithTag(ptr, 'mbed');
+    }
 }
 
 int64_t mbedtls_ms_time(void)
